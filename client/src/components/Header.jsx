fb34842eb3c112b58faf923d5c679fca0017f44f
Jingyu Huang <jhuan146@jhu.edu>
2022-09-23 11:45:27 -0400

    Add Zustand for state management

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,18 +4,23 @@ import {
     MediaQuery,
     Burger,
     Group,
+    useMantineTheme,
 } from "@mantine/core";
 import ToggleTheme from "./ToggleTheme";
+import useStore from "../services/store";
 
-function Header({ opened, setOpened, theme }) {
+function Header() {
+    const theme = useMantineTheme();
+    const { openSidebar, toggleOpenSidebar } = useStore();
+
     return (
         <MantineHeader height={70} p="md">
             <Group position="apart" style={{ height: "100%" }} spacing="xs">
                 <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
                     <MediaQuery largerThan="sm" styles={{ display: "none" }}>
                         <Burger
-                            opened={opened}
-                            onClick={() => setOpened((o) => !o)}
+                            opened={openSidebar}
+                            onClick={() => toggleOpenSidebar()}
                             size="sm"
                             color={theme.colors.gray[6]}
                             mr="xl"
